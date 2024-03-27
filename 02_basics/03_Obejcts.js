2fb74d98c7d1e61ae9870603a889e3c44008851a
GauravT-07 <gauravmaheshwari59840@gmail.com>
2024-03-27 18:15:24 +0530

    Add Object lesson

---
+++ 02_basics/03_Obejcts.js
@@ -0,0 +1,60 @@
+//singleton
+// object.create
+
+//object literals
+const person = {
+    name : ["bob","Smith"] ,
+    age : 32 ,
+    bio : function(){
+        console.log(`${this.name[0]} ${this.name[1]} is ${this.age} years old.`);
+    },
+    introduceSelf: function () {
+      console.log(`Hi! I'm ${this.name[0]}.`);
+    },
+};
+console.log(person.name[0]);
+person.name[0];
+person.age;
+person.bio();
+// "Bob Smith is 32 years old."
+person.introduceSelf();
+// "Hi! I'm Bob."
+console.log(typeof(person))
+// singleton
+// Object.create
+
+// object literals
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email = "hitesh@chatgpt.com"
+// Object.freeze(JsUser)   // Object will not be able change after this point.
+JsUser.email = "hitesh@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
