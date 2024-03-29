47cae4a0b5f264eb9459e310e5cd4b73316cbd11
GauravT-07 <gauravmaheshwari59840@gmail.com>
2024-03-29 18:08:46 +0530

    Add Arrow function class code

---
+++ 03_basics/01_arrow.js
@@ -0,0 +1,54 @@
+const user = {
+    username: "Gaurav",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+//console.log(this) //prints {}
+//if executed in browser console, It will give window object as in past time javascript engine was available only in browser
+
+// function chai(){
+//     let username = "hitesh"
+//     console.log(this.username);  // prints undefined // Reason - object properties will be handled by function using this
+// }
+
+// chai()
+
+// const chai = function () {
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+const chai =  () => {
+    let username = "hitesh"
+    console.log(this);
+}
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2
+
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+const addTwo = (num1, num2) => ({username: "hitesh"})
+
+
+console.log(addTwo(3, 4))
+
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
