fa5193de69c1d29799389c1664db5138c68a2ecb
GauravT-07 <gauravmaheshwari59840@gmail.com>
2024-05-18 18:30:13 +0530

    Getter setter changes

---
+++ 09_OOPs/getter_setter.js
@@ -0,0 +1,28 @@
+class User {
+    constructor(email,password){
+        this.email =email;
+        this.password=password
+    }
+     
+    get email(){
+        return this._email.toUpperCase()
+    }
+
+    set email(value)
+    {
+        this._email = value
+    }
+
+    get password(){
+          return `${this._password.toUpperCase()}gaurav`
+    }
+
+    set password(value){
+        this._password = value.toUpperCase() 
+    }
+}
+
+
+const gaurav = new User("h@abc.com","123")
+console.log(gaurav.password);
+console.log(gaurav.email);
